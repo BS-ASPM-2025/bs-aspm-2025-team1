4e01f852a42031239187bdfc1d68c8ab57ac0667
aymanelsayeed <aymanelsayeed@gmail.com>
2026-01-02 12:24:24 +0200

    bug fix.

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@ Model package initialization.
 """
 
 from models.job import Job
-from models.match import Match
+# from models.match import Match
 from models.resume import Resume
 
-__all__ = ["Job", "Match", "Resume"]
+__all__ = ["Job", "Resume"]
