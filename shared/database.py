62942a54cf786ec95b7f8d8ac575b8f16cb28be0
aymanelsayeed <aymanelsayeed@gmail.com>
2026-01-17 14:47:09 +0200

    add docstring.

--- shared/database.py
+++ shared/database.py
@@ -1,3 +1,8 @@
+"""
+
+Database setup and session management using SQLAlchemy.
+
+"""
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
@@ -13,6 +18,13 @@ Base = declarative_base()
 
 
 def get_db():
+    """
+    Dependency that provides a database session.
+    Ensures that the session is properly closed after use.
+    Yields:
+        db: SQLAlchemy Session
+
+    """
     Base.metadata.create_all(bind=engine)
     db = SessionLocal()
     try:
