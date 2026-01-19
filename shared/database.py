9c31cfddba6cbd4d2892364160eed5c7c0814789
Yurii Korolkov <uriko@ac.sce.ac.il>
2026-01-20 00:30:13 +0200

    all project

--- shared/database.py
+++ shared/database.py
@@ -1,21 +1,20 @@
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///my_database.db"
-#
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./my_database.db")
+
 engine = create_engine(DATABASE_URL, echo=True)
-
-
-# # Create session factory
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
 
-
 def get_db():
-    Base.metadata.create_all(bind=engine)
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
